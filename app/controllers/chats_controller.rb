561d39f78448a6fb10da6f767c7e222d0719ded5
Daktari <dindijjames@gmail.com>
2018-01-03 09:18:38 +0000

    Add create message

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,6 @@
-class ChatsController < ApplicationContrroller
+class ChatsController < ApplicationController
     before_action :require_login
     def index
+        @messages = Message.order(created_at: :asc)
     end
 end
